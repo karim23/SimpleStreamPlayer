7786c26bd331c6cb155d56cc0f9ee2f5844dbd14
karimskwdis <karim_mohamed@madarsoft.com>
2017-09-11 11:12:32 +0200

    Simple Streamplayer Using ExoPlayer

--- app/src/main/java/com/rubit/simplestreamplayer/ui/MainActivity.java
+++ app/src/main/java/com/rubit/simplestreamplayer/ui/MainActivity.java
@@ -23,12 +23,20 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		context = getApplicationContext();
 		setContentView(R.layout.activity_main);
-		contentUri = "http://abclive.abcnews.com/i/abc_live4@136330/index_1200_av-b.m3u8?sd=10&b=1200&rebase=on";
+		//		contentUri = "http://abclive.abcnews.com/i/abc_live4@136330/index_1200_av-b.m3u8?sd=10&b=1200&rebase=on";
+//		contentUri = "http://184.72.239.149/vod/smil:BigBuckBunny.smil/playlist.m3u8";
+
+		contentUri = "https://bitmovin-a.akamaihd.net/content/playhouse-vr/m3u8s/105560.m3u8";
+
+//		contentUri ="https://r4---sn-uxaxjvhxbt2u-5atr.googlevideo.com/videoplayback?key=yt6&itag=22";
+
 		// contentType = DemoUtil.TYPE_HLS;
 		final Intent intent = new Intent(context, VideoPlayerActivity.class).setData(Uri.parse(contentUri))
 				.putExtra(VideoPlayerActivity.CONTENT_ID_EXTRA, -1)
 				//Change the type according to the live streaming extension.
+//				.putExtra(VideoPlayerActivity.CONTENT_TYPE_EXTRA, DemoUtil.TYPE_MP4);
 				.putExtra(VideoPlayerActivity.CONTENT_TYPE_EXTRA, DemoUtil.TYPE_HLS);
+
 		liveStreamingTv =(TextView)findViewById(R.id.mainActivity_liveStreamingTv);
 		liveStreamingTv.setOnClickListener(new OnClickListener() {
 			
